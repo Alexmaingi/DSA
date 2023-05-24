94084720b7020a2c1e21a3b978d6de7fac0a7d92
Alexmaingi <alexmaingi077@gmail.com>
2023-05-24 08:51:21 +0300

    changes

---
+++ index.test.js
@@ -0,0 +1,5 @@
+const reverseString = require("./index");
+
+test("should have a function reveseString defined", () => {
+  expect(reverseString).toBeDefined();
+});
---
+++ index.ts
@@ -0,0 +1,18 @@
+// function chunkArr(arr: number[], size: number) {
+//   const chunk: number[] = [];
+//   for (let i = 0; i < arr.length; i += size) {
+//     chunk.push(arr.slice(i, i + size));
+//   }
+//   return chunk;
+// }
+
+// console.log(chunkArr([1, 2, 3, 3, 4, 5, 6, 7], 2));
+
+// class NewChunk {
+//   chunkArr(arr: number[], size: number) {
+//     const chunk: any = [];
+//     for (let i = 0; i < arr.length; i += size) {
+//       chunk.push(arr.slice(i, i + size));
+//     }
+//   }
+// }
